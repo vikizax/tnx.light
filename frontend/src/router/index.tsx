5a31a77d4a48a61b45dbebb452755e51ad9aa28e
Vivek Chetia <vikizax@Viveks-MacBook-Pro.local>
2024-11-06 02:56:12 +0530

    updated

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import DashboardLayout from "../layouts/dashboard";
 import TransactionsPage from "../pages/dashboard/transactions";
-import { ColorPalette } from "../utils/commons/color-palette";
 import NotFound from "../pages/not-found";
 export const router = createBrowserRouter([
   {
@@ -24,11 +23,7 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    path: "/test",
-    element: <div>THIS IS A TEST ROUTE!</div>,
-  },
-  {
     path: "*",
-    element: <NotFound />
+    element: <NotFound />,
   },
 ]);

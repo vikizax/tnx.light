191ddca395c8c26826ebc6304e96513b5a59433f
Vivek Chetia <vikizax@Viveks-MacBook-Pro.local>
2024-11-06 14:26:35 +0530

    updated utils

--- frontend/src/utils/result/index.ts
+++ frontend/src/utils/result/index.ts
@@ -1,18 +1,5 @@
 export type Result<T, E extends Error> = [T, null] | [null, E];
 
-export function intoResult<
-  T extends (...args: any[]) => any,
-  E extends Error = Error
->(cb: T, ...args: Parameters<T>): Result<ReturnType<T>, E> {
-  try {
-    const res = cb(...args) as ReturnType<T>;
-    return [res, null];
-  } catch (e) {
-    const err = e as E;
-    return [null, err];
-  }
-}
-
 export async function intoResultAsync<
   T extends (...args: any[]) => Promise<any>,
   E extends Error = Error
